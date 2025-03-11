3f56a72bcc5625bb75a0982c0a9006ab3d055906
Adrian Leon Diaz <alu0101495668@ull.edu.es>
2025-03-11 22:19:43 +0000

    ejercicios

--- EstaticoyFunciones/src/ejercicio-clase.ts
+++ EstaticoyFunciones/src/ejercicio-clase.ts
@@ -15,7 +15,7 @@ export function add(firstNumber: number, secondNumber: number): number {
  * Substracts two __numbers__ // hola
  * @param firstNumber - Consists of the first operand of the substraction
  * @param secondNumber - Consists of the second operand of the substraction
- * @returns The substraction of the two numbers `firstNumber` and `secondNumber`
+ * @returns The substraction of 2the two numbers `firstNumber` and `secondNumber`
  * ```typescript
  * sub(1, 7) = -6
  * ```
